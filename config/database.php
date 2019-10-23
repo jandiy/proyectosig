0386655d289d4ef25b7cdbdcf3fb6f5148601faf
jandiy <vargas11197@gmail.com>
2019-10-22 23:05:40 -0400

    data

--- config/database.php
+++ config/database.php
@@ -1,5 +1,6 @@
 <?php
-
+$DATABASE_URL = parse_url('postgres://sdykjarwaenggg:6f123734592b66f038a8351d7a702f65aba999c277775ab5690e3b1844010dc8@ec2-174-129-252-211.compute-1.amazonaws.com:5432/dfehm6i6fq73lt
+');
 return [
 
     /*
@@ -13,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -58,11 +59,16 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+           // 'host' => env('DB_HOST', '127.0.0.1'),
+           // 'port' => env('DB_PORT', '5432'),
+           // 'database' => env('DB_DATABASE', 'forge'),
+           // 'username' => env('DB_USERNAME', 'forge'),
+            //'password' => env('DB_PASSWORD', ''),
+          'host' => $DATABASE_URL['host'],
+          'port' => $DATABASE_URL['port'],
+          'database' => ltrim($DATABASE_URL["path"],"/"),
+          'username' => $DATABASE_URL['user'],
+          'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
