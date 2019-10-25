a5891acf462a31a5fefeb427b1cd55c38e64a94b
jandiy <vargas11197@gmail.com>
2019-10-24 20:53:48 -0400

    prueba post 1

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -25,4 +25,47 @@ class EstudianteController extends Controller
 
         return view('estudiantes.show',compact('estudiante','movil'));
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'nombre' => 'required',
+            'apellido' => 'required',
+            'genero'=> 'required', 
+            'fecha_nacimiento'=> 'required', 
+            'celular' => 'required',  
+            'contacto_emergencia' => 'required', 
+            'longitud' => 'required',
+            'latitud'=>'required',                    
+        ]);
+
+       // dd($request->input('longitud'));    
+        $usuario = new UsuarioMovil();
+        $usuario->nombre  = $request->input('nombre');
+        $usuario->apellido = $request->input('apellido');
+        $usuario->correo = $request->input('correo');
+        $usuario->contrasena = $request->input('contrasena');
+        $usuario->genero = $request->input('genero');
+        $usuario->fecha_nacimiento = $request->input('fecha_nacimiento');
+        $usuario->celular= $request->input('celular');
+        $usuario->contacto_emergencia= $request->input('contacto_emergencia');
+        $usuario->latitud= $request->input('latitud');
+        $usuario->longitud= $request->input('longitud');
+        if($request->hasFile('file'))
+          {
+
+           $filename= time().'_'.$request->file->getClientOriginalName(); 
+           $request->file->storeAs('public/upload','1569212052_img_msanoja_20160801-194152_imagenes_lv_getty.jpg');
+           $usuario->foto=$filename;
+          }
+        if($usuario->save()){
+            return response()->json($usuario);
+        }
+        else{
+            return response()->json('sorry',500);
+        }
+        
+
+          
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('ayudantelogin', 'AyudanteController@login');
+Route::post('estudiantepost', 'EstudianteController@store');
