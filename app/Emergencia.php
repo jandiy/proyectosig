5b8ffbcb87621bb57b3ca9287ca72206bf0017f1
jandiy <vargas11197@gmail.com>
2019-11-16 01:54:49 -0500

    en proceso 9

---
+++ app/Emergencia.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Emergencia extends Model
+{
+    protected $table = "trabajo";
+    protected $fillable = ['id','estado','hora','fecha','longitudA','latitudA','user_id','emergencia_id'];
+    public $timestamps=false;
+    protected $primaryKey = 'id';   
+}
--- resources/views/ayudantes/show.blade.php
+++ resources/views/ayudantes/show.blade.php
@@ -59,8 +59,8 @@
                 {{ $ayudante->latitud }}
                 <script>
                     var apikey = '5ac6a5f7d15b47b8a380b98684ae1885';
-                    var latitude = parseFloat(document.getElementById("latitud").value)||0;
-                    var longitude = parseFloat(document.getElementById("longitud").value)||0;
+                    var latitude = parseFloat(document.getElementById("latitud").value);
+                    var longitude = parseFloat(document.getElementById("longitud").value);
 
                     var api_url = 'https://api.opencagedata.com/geocode/v1/json'
 
